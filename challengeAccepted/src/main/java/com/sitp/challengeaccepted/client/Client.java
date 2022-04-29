abded8b9513d1ae5145e8803af09c7ca6a0ab83d
manuelM123 <83136070+manuelM123@users.noreply.github.com>
2022-04-29 11:12:46 +0100

    initialized interface design

--- challengeAccepted/src/main/java/com/sitp/challengeaccepted/client/Client.java
+++ challengeAccepted/src/main/java/com/sitp/challengeaccepted/client/Client.java
@@ -1,8 +1,13 @@
 package com.sitp.challengeaccepted.client;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.NumberBinding;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,10 +16,21 @@ public class Client extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxloader = new FXMLLoader(Client.class.getResource("login_register_menu.fxml"));
-        Scene scene = new Scene(fxloader.load(), 320, 240);
+        Scene scene = new Scene(fxloader.load(), 600, 400);
+        stage.setMinWidth(700);
+        stage.setMinHeight(400);
         stage.setTitle("Challenge Accepted");
         stage.setScene(scene);
         stage.show();
+
+        controller control = fxloader.getController();
+        control.gridCenter.setPrefWidth(150);
+        control.Login.setPrefWidth(150);
+        control.Registar.setPrefWidth(150);
+        //control.resize();
+
+        control.titleElement.wrappingWidthProperty().bind(scene.widthProperty().subtract(15));
+        //control.titleElement.minHeight((scene.getHeight()) - (scene.getHeight()))
     }
 
     public static void main(String[] args) {
--- challengeAccepted/src/main/java/com/sitp/challengeaccepted/client/controller.java
+++ challengeAccepted/src/main/java/com/sitp/challengeaccepted/client/controller.java
@@ -1,13 +1,27 @@
 package com.sitp.challengeaccepted.client;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 public class controller {
-    @FXML
-    private Text title;
+    public Text titleElement;
 
-    
+    public BorderPane borderPaneLogin;
+
+    public GridPane gridCenter;
+
+    public Button Login;
+
+    public Button Registar;
+
+    public Pane optionsPane;
+
+    public void resize(){
+        optionsPane.setMaxSize(2000,1000);
+    }
+
 
 }
